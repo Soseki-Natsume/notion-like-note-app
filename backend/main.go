3772b3eb353cae059fce3559d404bfe4d2321df7
Soseki-Natsume <florentins1919@gmail.com>
2026-09-13 18:01:50 +0900

    feat: registered some handlers, prepare template

---
+++ backend/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	_ "github.com/mattn/go-sqlite3" // sqldriverは実際に使用しないため、importして初期化のみする。
+)
+
+var db *sql.DB
+
+func main() {
+	var err error
+	db, err = sql.Open("sqlite3", "note.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	sqlStmt := `CREATE TABLE IF NOT EXISTS note (
+	id TEXT PRIMARY KEY, 
+	title TEXT NOT NULL, 
+	content TEXT NOT NULL DEFAULT '', 
+	created_at DATETIME NOT NULL, 
+	updated_at DATETIME NOT NULL
+	);`
+
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return
+	}
+
+	http.HandleFunc("/", listHundler)
+	http.HandleFunc("/create", createNoteHandler)
+	http.HandleFunc("/update", updateNoteHandler)
+	http.HandleFunc("/delete", deleteNoteHandler)
+	log.Println("Server running on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	/*tx, err := db.Begin()
+
+	if err != nil {
+		log.Fatal(err)
+	}*/
+
+}
---
+++ backend/noteHandler.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+const notitle = "無題"
+
+type Note struct {
+	ID        string    `json:"id"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+var listTmpl = template.Must(template.New("list").Parse(`<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>notion-like-note-app</title>
+</head>
+<body>
+    {{range .}}
+	<form id="" action="./read" method="get">
+    <button name="note-title" form=""></button>
+    </form>
+    <form id ="" action="./create" method="post">
+    <button name="create" form="">+新規作成</button>
+    </form>
+    <form id="" action="./delete" method="delete">
+    <button name="delete" form="">-削除</button>
+    </form>
+	{{- end}}    
+    <label for="title">タイトル</label>
+    <textarea name="title"></textarea>
+    <label for="content">内容</label>
+    <textarea name="content"></textarea>
+</body>
+</html>
+`))
+
+func listHundler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	rows, err := db.Query("SELECT title, content FROM note ORDER BY updated_at DESC")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var notes []Note
+	for rows.Next() {
+		var n Note
+		if err := rows.Scan(&n.Title, &n.Content); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		notes = append(notes, n)
+	}
+	w.Header().Set("Content-Type", "text/html; charset=utf8")
+	err = listTmpl.Execute(w, notes)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func createNoteHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	newID := uuid.New().String()
+	now := time.Now()
+
+	sqlStr := "INSERT INTO note(id, title, content, created_at, updated_at) VALUES(?, ?, ?, ?, ?)"
+	_, err := db.Exec(sqlStr, newID, notitle, "", now, now)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, `{"id": "%s", "title": "notitle", "content": ""}`, newID)
+}
+
+func updateNoteHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPut {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var note Note
+	err := json.NewDecoder(r.Body).Decode(&note)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	now := time.Now()
+
+	sqlStr := "UPDATE note SET title = ?, content = ?, updated_at = ? WHERE id = ?"
+	_, err = db.Exec(sqlStr, note.Title, note.Content, now, note.ID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, `{"status": "success"}`)
+}
+
+func deleteNoteHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodDelete {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+	}
+
+	var note Note
+	err := json.NewDecoder(r.Body).Decode(&note)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	sqlStr := "DELETE FROM note WHERE id = ?"
+	_, err = db.Exec(sqlStr, note.ID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, `{"status": "success"}`)
+}
